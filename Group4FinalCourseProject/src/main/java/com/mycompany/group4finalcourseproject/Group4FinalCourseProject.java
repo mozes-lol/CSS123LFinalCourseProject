d0f0199c3ee1bf901f3796506e9fd890f1baf6de
mozes-lol <63577510+mozes-lol@users.noreply.github.com>
2024-10-03 08:32:32 +0800

    Create Project

---
+++ Group4FinalCourseProject/src/main/java/com/mycompany/group4finalcourseproject/Group4FinalCourseProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.group4finalcourseproject;
+
+/**
+ *
+ * @author johnm
+ */
+public class Group4FinalCourseProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
