7fd538e2a0112de650992f69ec92f08cbb1087e4
mozes-lol <johnmarkgarcia164@gmail.com>
2024-10-11 09:09:48 +0800

    create SalaryDetails.java

---
+++ Group4FinalCourseProject/src/main/java/com/group4/libraries/SalaryDetails.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.group4.libraries;
+
+/**
+ *
+ * @author John Mark Garcia
+ */
+public interface SalaryDetails {
+    int salary = 0; //I'm not sure if declaring variables in an interface works; Just giving this one a shot
+    public void computeSalary();
+}
