3452620e755210579165420bc7fd63ebf18be06a
Garpss <156627866+Garpss@users.noreply.github.com>
2024-10-11 22:43:00 +0800

    Update FullTimer.java

--- Group4FinalCourseProject/src/main/java/com/group4/libraries/FullTimer.java
+++ Group4FinalCourseProject/src/main/java/com/group4/libraries/FullTimer.java
@@ -8,7 +8,17 @@ package com.group4.libraries;
  *
  * @author garps
  */
-public class FullTimer {
+public class FullTimer extends Employee  implements SalaryDetails{
     private String assignedOffice;
+    private final double monthSalary = 50000;
+
+    public FullTimer(String assignedOffice, String name) {
+        super(name);
+        this.assignedOffice = assignedOffice;
+    }
+
+   public void computeSalary(){
+       
+    }
     
 }
