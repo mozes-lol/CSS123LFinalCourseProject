d669df41b5a6fae12c550740f39bc114d4acd9ba
mozes-lol <johnmarkgarcia164@gmail.com>
2024-10-14 23:02:32 +0800

    PartTimer - Create methods

--- Group4FinalCourseProject/src/main/java/com/group4/libraries/PartTimer.java
+++ Group4FinalCourseProject/src/main/java/com/group4/libraries/PartTimer.java
@@ -10,29 +10,22 @@ package com.group4.libraries;
  */
 public class PartTimer extends Employee implements SalaryDetails {
     private String primaryAlternativeOccupation;
-    private final double hourlyRate = 500;// initialize ko for easy logic pwede rin tanggalin if decided na manually input yung hourly rate...
-    private int hourWork;//DINAGDAG KO LANG kasi i trhink mas doable skain pero if may mas oaky kayu method feel free to suggest
 
     public String getPrimaryAlternativeOccupation() {
         return primaryAlternativeOccupation;
     }
 
-    public PartTimer(String primaryAlternativeOccupation, int hourWork, String name) {
+    public PartTimer(String primaryAlternativeOccupation, String name) {
         super(name);
         this.primaryAlternativeOccupation = primaryAlternativeOccupation;
-        this.hourWork = hourWork;
     }
 
     @Override
-    public void computeSalary(){
-        double Salary = hourlyRate * hourWork;
-        System.out.println(name + "'s computed Salary is:" + Salary);
+    public String computeSalary(String salaryAmount){
+        return salaryAmount;
     }
     
-    
-    
-    
     public void  writeEmployeeDescription(){
-        // same dito di ko alam ginagawa pero ang iniisip ko lang dito is output of description correct me if im wrong babaguhin ko nalang
+
     }
 }
