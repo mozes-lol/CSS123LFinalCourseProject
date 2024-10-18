d8bc316aaeb7c653f75351c5cf179b1819c5fbd0
mozes-lol <63577510+mozes-lol@users.noreply.github.com>
2024-10-18 20:00:30 +0800

    Import libraries

--- Group4FinalCourseProject/src/main/java/com/group4/applications/AddEmployee.java
+++ Group4FinalCourseProject/src/main/java/com/group4/applications/AddEmployee.java
@@ -8,6 +8,8 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
+import com.group4.libraries.Employee;
+import com.group4.libraries.EmployeeList;
 
 /**
  *
