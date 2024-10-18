c113657c67c29e84ad71e1963bae3def0725782e
mozes-lol <63577510+mozes-lol@users.noreply.github.com>
2024-10-18 19:58:17 +0800

    Enable Show.java on program startup
    From now on, just press F6 and it should start the program will the form application appearing

--- Group4FinalCourseProject/src/main/java/com/group4/applications/Startup.java
+++ Group4FinalCourseProject/src/main/java/com/group4/applications/Startup.java
@@ -13,15 +13,17 @@ import com.group4.libraries.EmployeeList;
 public class Startup {
     public static void main(String[] args) {
         // this is for testing only
-        EmployeeList employeeList = new EmployeeList();
-        employeeList.newFullTimerEmployee("A", "B", "C", true, 85, "500");
-        employeeList.newFullTimerEmployee("A", "B", "C", true, 85, "500");
-        employeeList.newFullTimerEmployee("A", "B", "C", true, 85, "500");
-        employeeList.newPartTimerEmployee("A", "B", "C", true, 85, "500");
-        employeeList.newPartTimerEmployee("A", "B", "C", true, 85, "500");
-        employeeList.newPartTimerEmployee("A", "B", "C", true, 85, "500");
-        employeeList.newFullTimerEmployee("A", "B", "C", true, 85, "500");
-        employeeList.newFullTimerEmployee("A", "B", "C", true, 85, "500");
-        System.out.println(employeeList.getEmployeeList());
+//        EmployeeList employeeList = new EmployeeList();
+//        employeeList.newFullTimerEmployee("A", "B", "C", true, 85, "500");
+//        employeeList.newFullTimerEmployee("A", "B", "C", true, 85, "500");
+//        employeeList.newFullTimerEmployee("A", "B", "C", true, 85, "500");
+//        employeeList.newPartTimerEmployee("A", "B", "C", true, 85, "500");
+//        employeeList.newPartTimerEmployee("A", "B", "C", true, 85, "500");
+//        employeeList.newPartTimerEmployee("A", "B", "C", true, 85, "500");
+//        employeeList.newFullTimerEmployee("A", "B", "C", true, 85, "500");
+//        employeeList.newFullTimerEmployee("A", "B", "C", true, 85, "500");
+//        System.out.println(employeeList.getEmployeeList());
+        Show employeeListUI = new Show();
+        employeeListUI.setVisible(true);
     }
 }
