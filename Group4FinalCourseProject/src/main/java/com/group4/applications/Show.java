1d28a5470198f05f87c79fef95ec2915337b39c7
mozes-lol <johnmarkgarcia164@gmail.com>
2024-10-24 16:39:54 +0800

    Make the employee list form persistent

--- Group4FinalCourseProject/src/main/java/com/group4/applications/Show.java
+++ Group4FinalCourseProject/src/main/java/com/group4/applications/Show.java
@@ -200,7 +200,6 @@ public class Show extends javax.swing.JFrame implements ActionListener{
     }//GEN-LAST:event_jButton_RefreshListActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        close();
         AddEmployee pi = new AddEmployee();
         pi.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
